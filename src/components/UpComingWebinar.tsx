b50a5a110cb0a234a34530f8fa205c68a149edbc
dishant987 <tankdishant2003@gmail.com>
2024-05-28 01:01:33 +0530

    change

--- src/components/UpComingWebinar.tsx
+++ src/components/UpComingWebinar.tsx
@@ -70,7 +70,7 @@ const UpComingWebinar = () => {
           }
         ))} />
         </div>
-        <div className="mt-5 text-center">
+        <div className="mt-2 mb-7 text-center">
           <Link href={"#"}>
             <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-2xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               View All Webinars
